4b9e3444a64077667834c32c47cb42578f6e49b9
Surya <suryaunder18@gmail.com>
2026-05-03 23:48:10 +0530

    Some Version

--- script1.js
+++ script1.js
@@ -1,2 +1,13 @@
-const name = 'Spidey';
-console.log(name);
+// let a = ["a", "b", "c", "d", "e"];
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+let a = {
+    name : "John",
+    age : 30,
+    city : "New York"
+}
+a.name = "Jane";
+console.log(a.name);
+let b = [ {name : "Surya", age : 22}, {name : "Siva", age : 25}];
+console.log(b[0].name);
