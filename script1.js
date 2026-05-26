f6c0d0b80f3fcb2fec5c82a47a41eba64891c5bc
Surya <suryaunder18@gmail.com>
2026-05-26 23:22:29 +0530

    Hot Intake

--- script1.js
+++ script1.js
@@ -2,12 +2,17 @@
 // for (let i = 0; i < a.length; i++) {
 //     console.log(a[i]);
 // }
-let a = {
-    name : "John",
-    age : 30,
-    city : "New York"
-}
-a.name = "Jane";
-console.log(a.name);
-let b = [ {name : "Surya", age : 22}, {name : "Siva", age : 25}];
-console.log(b[0].name);
+// let a = {
+//     name : "John",
+//     age : 30,
+//     city : "New York"
+// }
+// a.name = "Jane";
+// console.log(a.name);
+// let b = [ {name : "Surya", age : 22}, {name : "Siva", age : 25}];
+// console.log(b[0].name);
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
