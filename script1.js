5c12716d641c3943980bb1befc34a5577efcb27a
Surya <suryaunder18@gmail.com>
2026-05-26 23:33:32 +0530

    Add Comment

--- script1.js
+++ script1.js
@@ -12,14 +12,14 @@
 // let b = [ {name : "Surya", age : 22}, {name : "Siva", age : 25}];
 // console.log(b[0].name);
 
-import express from "express";
-const app = express();
+import express from "express";  // Importing the Express library to create a web server
+const app = express();      // Creating an instance of the Express application
 const port = 3000;
 
 app.get("/", (req,res) => {
-    res.send("Hello World");
+    res.send("Hello World");   //  Sending "Hello World" as a response via GET method
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);  // Hosting a server on port 3000 via LISTEN method
 });
