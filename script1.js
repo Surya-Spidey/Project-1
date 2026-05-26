90f7e9f1e5df123ff712691a91854d7ba02998c0
Surya <suryaunder18@gmail.com>
2026-05-26 23:30:45 +0530

    Learn Express

--- script1.js
+++ script1.js
@@ -12,7 +12,14 @@
 // let b = [ {name : "Surya", age : 22}, {name : "Siva", age : 25}];
 // console.log(b[0].name);
 
-const express = require('express');
+import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req,res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
